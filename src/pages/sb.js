9eb8a91808f495934f58dfa939dbffe533151c03
farooq <145667687+farucq@users.noreply.github.com>
2025-10-16 12:42:55 +0530

    Add Companies Page component in sb.js

---
+++ src/pages/sb.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function sb() {
+  return (
+    <div style={{backgroundColor:"red",height:"100vh"}}>
+      <h1>Companies Page</h1>
+    </div>
+  )
+}
+
+export default sb
